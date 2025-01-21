96e58a3b73b176be6d219c8bfbefea1f266c3b20
Luxxinator1 <lukas.koenig.regensburg@protonmail.com>
2025-01-21 19:13:34 +0100

    Add input system with error handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        println!("Please input the nth Fibonacci number you want to know: ");
+
+        let mut n = String::new();
+
+        io::stdin()
+            .read_line(&mut n)
+            .expect("Please input a positive number!");
+
+        let _n: u64 = match n.trim().parse() {
+            Ok(n) => n,
+            Err(_) => {
+                println!("{n} is not a valid number!");
+                continue;
+            }
+        };
+    }
 }
